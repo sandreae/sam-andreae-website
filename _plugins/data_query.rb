078ebfcc786810e10392ef828ca0b3896a7e7fba
sandreae <git@contact.samandreae.com>
2021-04-30 00:41:45 +0100

    More fluid filters and data

---
+++ _plugins/data_query.rb
@@ -0,0 +1,56 @@
+require "json"
+require "yaml"
+
+module Jekyll
+  module DataQuery
+    def _people()
+      people_file = File.open File.expand_path('links/people.yml')
+      return YAML.load(people_file)
+    end
+
+    def _places()
+      places_file = File.open(File.expand_path('links/places.yml'))
+      return YAML.load(places_file)
+    end
+  
+    def _gigs()
+      gigs_file = File.open(File.expand_path('links/gigs.yml'))
+      return YAML.load(gigs_file)
+    end
+        
+    def url(input)
+      people = self._people()
+      places = self._places()
+
+      people.each do |key, value|
+        input.gsub! key, "<a href=\"https://#{value}\">#{key}</a>"
+      end
+      
+      places.each do |key, value|
+        input.gsub! key, "<a href=\"https://#{places[key]["url"]}\">#{key}</a>"
+      end
+
+      return input
+    end
+    
+    def venue(input)
+      places = self._places()
+  
+      if places[input]
+        return "<a href=\"https://#{places[input]["url"]}\">#{input}</a>, #{places[input]["city"]}"
+      else
+        return input
+      end
+    end
+    
+    def gigs(input)
+      string = ""
+      self._gigs().each do |gig|
+        string += "|#{gig["date"]}|#{self.url(gig["group"])}|#{self.venue(gig["venue"])}|\n"
+      end
+      return string
+    end
+  end
+end
+  
+Liquid::Template.register_filter(Jekyll::DataQuery)
